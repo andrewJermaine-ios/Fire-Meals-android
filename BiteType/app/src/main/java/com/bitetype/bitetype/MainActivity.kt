5991366c9a0ddf1bb8b354cc13d29aa0dc885eab
andrewjermaine-ios <andrewjermaine.ios@gmail.com>
2023-08-14 13:25:05 -0700

    Added text to image ai abilities to macro meals Android.

--- BiteType/app/src/main/java/com/bitetype/bitetype/MainActivity.kt
+++ BiteType/app/src/main/java/com/bitetype/bitetype/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.bitetype.bitetype
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -15,11 +20,15 @@ import com.aallam.openai.api.chat.ChatCompletionRequest
 import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
 import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.api.image.ImageCreation
+import com.aallam.openai.api.image.ImageSize
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
 import com.bitetype.bitetype.ui.theme.BiteTypeTheme
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.net.URLEncoder
+import java.util.concurrent.Executors
 import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +37,7 @@ class MainActivity : ComponentActivity() {
     lateinit var textInput: EditText
     lateinit var goButton: Button
     lateinit var responseView: TextView
+    lateinit var imageResponseView: ImageView
 
    private val openai = OpenAI(
         token = "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",
@@ -42,9 +52,7 @@ class MainActivity : ComponentActivity() {
         textInput = findViewById(R.id.aiTextField)
         goButton = findViewById(R.id.button)
         responseView = findViewById(R.id.myTextView)
-
-
-
+        imageResponseView = findViewById(R.id.aiResponseImageView)
 
         goButton.setOnClickListener {
             Toast.makeText(this, textInput.text, Toast.LENGTH_SHORT).show()
@@ -61,7 +69,6 @@ class MainActivity : ComponentActivity() {
                     )
                 )
             )
-// or, as flow
             GlobalScope.launch {
                 val fullResponse = openai.chatCompletion(chatCompletionRequest)
 
@@ -69,9 +76,42 @@ class MainActivity : ComponentActivity() {
                 println("--------------------------------------!!!!!!!!!!!!!!!!")
                 println(fullResponse.choices[0].message!!?.content)
                 responseView.text = fullResponse.choices[0].message!!?.content
+
+                val executor = Executors.newSingleThreadExecutor()
+                val handler  = Handler(Looper.getMainLooper())
+                var image: Bitmap? = null
+
+                executor.execute {
+                    GlobalScope.launch {
+                        val images = openai.imageURL(
+                            creation = ImageCreation(
+                                prompt = textInput.text.toString(), //fullResponse.choices[0].message!!?.content!!,
+                                n = 2,
+                                size = ImageSize.is1024x1024
+                            )
+                        )
+                        val image1 = images[0]
+                       //tester  var newImage = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-AJiV79nllE1S9OyTXz5XCpbQ/user-oPnS2YGdPYkSg4cQxmkEZsqp/img-hX8tCM1UqaeWZPzrBiBfWG3C.png?st=2023-08-14T19%3A05%3A59Z&se=2023-08-14T21%3A05%3A59Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-08-14T17%3A24%3A14Z&ske=2023-08-15T17%3A24%3A14Z&sks=b&skv=2021-08-06&sig=1RQoURxAP2UAeOb/ejmwTH7%2BNQ3RSu/0r6rvmaVNHdQ%3D"
+                        println(image1.url)
+
+                        try {
+                            val `in` = java.net.URL(image1.url).openStream()
+                            image = BitmapFactory.decodeStream(`in`)
+
+                            handler.post {
+                                imageResponseView.setImageBitmap(image)
+                            }
+                        }
+                        catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                }
             }
             //Toast.makeText(this, fullResponse.toString(), Toast.LENGTH_SHORT).show()
-
+        }
+    }
+    @OptIn(BetaOpenAI::class)
+    fun createImage(fromPrompt: String)  {
 
 
         }
